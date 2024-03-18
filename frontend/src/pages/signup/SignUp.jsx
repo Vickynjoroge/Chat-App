efe3cb797bf2d31e64de3f31967a395831b3800e
Victor Njoroge <vnjorogen2001@gmail.com>
2024-03-18 15:37:24 +0300

    updated file

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom"
 import GenderCheckbox from "./GenderCheckbox"
 import { useState } from "react"
+import useSignup from "../../hooks/useSignup"
 
 const SignUp = () => {
 
   const [inputs,  setInputs] = useState({
-    fullname: "",
+    fullName: "",
     username: "",
     password: "",
     confirmPassword: "",
     gender: ""
   })
 
+  const {loading,signup} = useSignup()
+ 
   const handleCheckboxChange = (gender) => {
     setInputs({...inputs,gender})
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault()
-    console.log(inputs)
-  }
+    await signup(inputs)
+  };
 
     return <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
         <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -37,8 +40,8 @@ const SignUp = () => {
             type="text" 
             placeholder="John Doe" 
             className="w-full input input-bordered h-10"
-            value={inputs.fullname}
-            onChange={(e) => setInputs({ ...inputs, fullname: e.target.value })}
+            value={inputs.fullName}
+            onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
             />
           </div>
 
